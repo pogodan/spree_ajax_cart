9d30bfaedff370c5d315c5825b8142db4c0076c2
Paul <paul@pogodan.com>
2011-02-25 00:51:39 +0100

    after a redirect FF.js & Rails are no longer happy, so let's render straight away

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -7,7 +7,20 @@ OrdersController.class_eval do
     flash[:notice] = "Added #{variant.name} to cart"
 
     respond_to do | format |
-      format.js {render :action => 'edit', :layout => false}  
+      format.js { render :action => 'edit' }  
+    end
+  end
+  
+  def update
+    @order = current_order
+    if @order.update_attributes(params[:order])
+      @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
+      respond_to do |format|
+        format.html { redirect_to cart_path }
+        format.js   { render :action => 'edit' }  
+      end
+    else
+      render :edit
     end
   end
 end
